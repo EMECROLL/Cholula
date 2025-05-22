64682e932d503162b312203f0d1c478995968e68
Manuel Alejandro Pasos Cupul <alexito210904@gmail.com>
2025-05-22 16:21:32 -0500

    Feat: About Cholula Section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import HeroComponent from '@/components/HeroComponent.vue';
+import AboutCholulaComponent from '@/components/AboutCholulaComponent.vue';
 </script>
 
 <template>
   <HeroComponent />
+  <AboutCholulaComponent />
 </template>
